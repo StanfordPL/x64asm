1fcc03160d63ed0b0b65a4231088ebfa66dd86b6
eric <eric.schkufza@gmail.com>
2013-01-27 17:02:24 -0800

    Fixed up constants test.

--- test/constants.cc
+++ test/constants.cc
@@ -6,10 +6,6 @@
 using namespace x64asm;
 using namespace std;
 
-void foo() {
-	cout << "Hello world!  You called this from assembly!" << endl;
-}
-
 template <typename T>
 void all(const vector<T>& ts) {
 	for ( const auto& t : ts )
@@ -20,10 +16,7 @@ void all(const vector<T>& ts) {
 	cout << endl;
 }
 
-int main() {
-	cout << Syntax::ATT;
-
-	cout << "BEGIN WRITING CONSTANTS" << endl;
+void all() {
 	all(crs);
 	all(drs);
 	all(eflags);
@@ -39,39 +32,18 @@ int main() {
 	all(sts);
 	all(xmms);
 	all(ymms);
-	cout << "DONE WRITING CONSTANTS" << endl;
+}
 
-	cout << "Parent register for " << xmm1 << " = " << xmm1.parent() << endl;
-	cout << "Parent register for " << al << " = " << al.parent() << endl;
-	cout << "Parent register for " << ah << " = " << ah.parent() << endl;
-	cout << "Parent register for " << ax << " = " << ax.parent() << endl;
-	cout << "Parent register for " << eax << " = " << eax.parent() << endl;
-	cout << "Parent register for " << rax << " = " << rax.parent() << endl;
+int main() {
+	cout << "AT&T Constants:" << endl;
+	cout << Syntax::ATT;
+	all();
+	cout << endl;
 
-	OpSet os = OpSet::empty();
-	os += al;
-	os += ah;
-	os += ax;
-	os += eax;
-	os += rax;
-
-	os += ch;
-	os += dl;
-	os += spl;
-	os += mm3;
-	os += xmm2;
-	os += ymm2;
-	os += ymm4;
-
-	os += af;
-
-	cout << os << endl;
-	cout << OpSet::universe() << endl;
-
-	cout << "NOW FOR SOME CODE" << endl << endl;
-
-	Code c { Instruction{Opcode::FABS}, Instruction{Opcode::FABS}, Instruction{Opcode::FABS}, Instruction{Opcode::FABS}, Instruction{Opcode::RET} };
-	cout << c << endl;
+	cout << "Intel Constants:" << endl;
+	cout << Syntax::INTEL;
+	all();
+	cout << endl;
 
 	return 0;
 }
