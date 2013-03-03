070a65dd5387c4e20edda560ad57527928361cbc
eric <eric.schkufza@gmail.com>
2013-03-02 18:23:47 -0800

    Documentation.

--- tools/x64asm.cc
+++ tools/x64asm.cc
@@ -6,16 +6,22 @@
 using namespace std;
 using namespace x64asm;
 
+/** Prints a usage error. */
 int usage_error() {
 	cerr << "Usage: x64asm -i (att|intel) -o (att|intel|hex)" << endl;
 	return 1;
 }
 
+/** Prints a parse error. */
 int parse_error() {
 	cerr << "Unable to read input file!" << endl;
 	return 2;
 }
 
+/** A simple test program. Reads inputs in either at&t or intel syntax
+	  and writes results in either at&t or intel syntax or as human-readable
+		hex.
+*/
 int main(int argc, char** argv) {
 	if ( argc != 5 )
 		return usage_error();
