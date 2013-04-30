1faf47622087f50065ab850c81e41fdbdd24fc82
eric schkufza <eric.schkufza@gmail.com>
2013-04-30 15:26:53 -0700

    Redirecting g++ warnings to /dev/null in test script.

--- test/test.sh
+++ test/test.sh
@@ -16,7 +16,7 @@ do
 	while read line           
 	do           
     echo $line > test.s
-		g++ -c test.s
+		g++ -c test.s 2> /dev/null
 
 		objdump -d test.o | tail -n+8 | cut -c 7-27 | sed 'N;s/\n//' | sed 's/ *$//' > objdump.out
 		cat test.s | ../bin/x64asm -i att -o hex | sed 'N;s/\n//' | sed 's/ *$//' > x64asm.out
