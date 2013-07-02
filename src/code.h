24a87666d16f5091739c4e9c5b3e490cf1e9ba1d
eric schkufza <eric.schkufza@gmail.com>
2013-07-02 16:55:06 -0700

    Bug fix for code printing.`

--- src/code.h
+++ src/code.h
@@ -84,10 +84,11 @@ inline bool Code::check() const {
 inline std::ostream& Code::write_att(std::ostream& os) const {
   for (size_t i = 0, ie = size(); i < ie; ++i) {
     (*this)[i].write_att(os);
-    if (i + 1 != ie) {
+    if (i+1 != ie) {
       os << std::endl;
     }
   }
+  return os;
 }
 
 } // namespace x64asm
