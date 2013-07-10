fb7bfb5f29c11a04a9d2cc74482af05bb70e06e2
eric schkufza <eric.schkufza@gmail.com>
2013-07-10 13:54:52 -0700

    Updated test script for new test driver usage.

--- test/test.sh
+++ test/test.sh
@@ -19,7 +19,7 @@ do
 		g++ -c test.s 2> /dev/null
 
 		objdump -d test.o | tail -n+8 | cut -c 7-27 | sed 'N;s/\n//' | sed 's/ *$//' > objdump.out
-		cat test.s | ../bin/x64asm -i att -o hex | sed 'N;s/\n//' | sed 's/ *$//' > x64asm.out
+		cat test.s | ../bin/x64asm | sed 'N;s/\n//' | sed 's/ *$//' > x64asm.out
 
 		diff objdump.out x64asm.out > /dev/null
 		if [ $? -ne 0 -a -s objdump.out ]
