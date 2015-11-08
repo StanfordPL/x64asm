38c0ad0155baf0fd9a95a9ec5f0c8b2a0abb36c3
Stefan Heule <stefanheule@gmail.com>
2015-11-07 23:10:36 -0800

    add width for type

--- src/operand.cc
+++ src/operand.cc
@@ -4,6 +4,7 @@
 
 #include "src/fail.h"
 #include "src/imm.h"
+#include "src/type.h"
 #include "src/m.h"
 #include "src/operand.h"
 #include "src/r.h"
@@ -13,54 +14,7 @@ using namespace std;
 using namespace cpputil;
 
 uint16_t Operand::size() const {
-
-  switch(type()) {
-  case Type::IMM_8:
-  case Type::ZERO:
-  case Type::ONE:
-  case Type::THREE:
-  case Type::M_8:
-  case Type::MOFFS_8:
-  case Type::RH:
-  case Type::R_8:
-  case Type::AL:
-  case Type::CL:
-    return 8;
-
-  case Type::IMM_16:
-  case Type::M_16:
-  case Type::R_16:
-  case Type::MOFFS_16:
-  case Type::AX:
-  case Type::DX:
-    return 16;
-
-  case Type::IMM_32:
-  case Type::MOFFS_32:
-  case Type::M_32:
-  case Type::R_32:
-  case Type::EAX:
-    return 32;
-
-  case Type::IMM_64:
-  case Type::MOFFS_64:
-  case Type::M_64:
-  case Type::R_64:
-  case Type::RAX:
-    return 64;
-
-  case Type::XMM:
-  case Type::XMM_0:
-  case Type::M_128:
-    return 128;
-
-  case Type::YMM:
-  case Type::M_256:
-    return 256;
-
-  default:
-    return 0;
-  }
+  return bit_width_of_type(type());
 }
 
 bool Operand::is_gp_register() const {
---
+++ src/type.cc
@@ -0,0 +1,92 @@
+/*
+Copyright 2013-2015 Stanford University
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include "src/type.h"
+
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+namespace x64asm {
+
+uint16_t bit_width_of_type(Type t) {
+  switch (t) {
+  case x64asm::Type::IMM_8: return 8;
+  case x64asm::Type::IMM_16: return 16;
+  case x64asm::Type::IMM_32: return 32;
+  case x64asm::Type::IMM_64: return 64;
+  case x64asm::Type::ZERO: return 0;
+  case x64asm::Type::HINT: return 0;
+  case x64asm::Type::ONE: return 0;
+  case x64asm::Type::THREE: return 0;
+  case x64asm::Type::LABEL: return 0;
+  case x64asm::Type::M_8: return 8;
+  case x64asm::Type::M_16: return 16;
+  case x64asm::Type::M_32: return 32;
+  case x64asm::Type::M_64: return 64;
+  case x64asm::Type::M_128: return 128;
+  case x64asm::Type::M_256: return 256;
+  case x64asm::Type::M_16_INT: return 16;
+  case x64asm::Type::M_32_INT: return 32;
+  case x64asm::Type::M_64_INT: return 64;
+  case x64asm::Type::M_32_FP: return 32;
+  case x64asm::Type::M_64_FP: return 64;
+  case x64asm::Type::M_80_FP: return 80;
+  case x64asm::Type::M_80_BCD: return 80;
+  case x64asm::Type::M_2_BYTE: return 2;
+  case x64asm::Type::M_28_BYTE: return 28;
+  case x64asm::Type::M_108_BYTE: return 108;
+  case x64asm::Type::M_512_BYTE: return 512;
+  case x64asm::Type::FAR_PTR_16_16: return 0;
+  case x64asm::Type::FAR_PTR_16_32: return 0;
+  case x64asm::Type::FAR_PTR_16_64: return 0;
+  case x64asm::Type::MM: return 64;
+  case x64asm::Type::MOFFS_8: return 8;
+  case x64asm::Type::MOFFS_16: return 16;
+  case x64asm::Type::MOFFS_32: return 32;
+  case x64asm::Type::MOFFS_64: return 64;
+  case x64asm::Type::PREF_66: return 0;
+  case x64asm::Type::PREF_REX_W: return 0;
+  case x64asm::Type::FAR: return 0;
+  case x64asm::Type::RH: return 8;
+  case x64asm::Type::AL: return 8;
+  case x64asm::Type::CL: return 8;
+  case x64asm::Type::R_8: return 8;
+  case x64asm::Type::AX: return 16;
+  case x64asm::Type::DX: return 16;
+  case x64asm::Type::R_16: return 16;
+  case x64asm::Type::EAX: return 32;
+  case x64asm::Type::R_32: return 32;
+  case x64asm::Type::RAX: return 64;
+  case x64asm::Type::R_64: return 64;
+  case x64asm::Type::REL_8: return 8;
+  case x64asm::Type::REL_32: return 32;
+  case x64asm::Type::FS: return 0;
+  case x64asm::Type::GS: return 0;
+  case x64asm::Type::SREG: return 0;
+  case x64asm::Type::ST_0: return 0;
+  case x64asm::Type::ST: return 0;
+  case x64asm::Type::XMM_0: return 128;
+  case x64asm::Type::XMM: return 128;
+  case x64asm::Type::YMM: return 256;
+  default:
+    assert(false);
+  }
+  return 0;
+}
+
+} // namespace x64asm
